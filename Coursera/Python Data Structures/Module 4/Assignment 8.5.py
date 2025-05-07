a88cda733a0e6717773ff279c566a9af65c64888
Ace Gamitin <gamitinacevincent@gmail.com>
2025-05-07 22:23:46 +0800

    #

--- Coursera/Python Data Structures/Module 4/Assignment 8.5.py
+++ Coursera/Python Data Structures/Module 4/Assignment 8.5.py
@@ -5,4 +5,13 @@ if len(fname) < 1:
 fh = open(fname)
 count = 0
 
+for line in fh:
+    if line.startswith("From "):
+        read = line.rstrip().split()
+        email = read[1]
+        print(email)
+        count +=1
+    else:
+        continue
+
 print("There were", count, "lines in the file with From as the first word")
