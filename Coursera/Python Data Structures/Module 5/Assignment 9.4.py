4f5c881d43e2de666e5768ba0efd3d2fb5ff3f58
Ace Gamitin <gamitinacevincent@gmail.com>
2025-05-07 22:34:23 +0800

    #

--- Coursera/Python Data Structures/Module 5/Assignment 9.4.py
+++ Coursera/Python Data Structures/Module 5/Assignment 9.4.py
@@ -2,3 +2,20 @@ name = input("Enter file:")
 if len(name) < 1:
     name = "mbox-short.txt"
 handle = open(name)
+
+dict = {}
+
+for lines in handle:
+    if lines.startswith("From "):
+        words = lines.split()
+        email = words[1]
+        dict[email] = dict.get(email, 0)+1
+                   
+i = None
+j = None
+
+for k, v in dict.items():
+    if j is None or j < v:
+        j = v
+        i = k
+print(i, j)
