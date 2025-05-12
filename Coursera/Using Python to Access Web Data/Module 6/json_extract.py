683407e9f8e853e95ac9e7832e9fa6b4692555bd
Ace Gamitin <gamitinacevincent@gmail.com>
2025-05-12 21:32:08 +0800

    #

--- Coursera/Using Python to Access Web Data/Module 6/json_extract.py
+++ Coursera/Using Python to Access Web Data/Module 6/json_extract.py
@@ -1,15 +1,15 @@
 import json
-import requests
+import urllib.request
 
 url = "https://py4e-data.dr-chuck.net/comments_2198753.json"
 
-response = requests.get(url)
-data = response.json()
+response = urllib.request.urlopen(url)
+data = response.read().decode()
 
 num_list = []
 
-for comment in data['comments']:
+'''for comment in data['comments']:
     # comment is a dict; get its 'count'
-    num_list.append(comment['count'])
+    num_list.append(comment['count'])'''
 
 print(sum(num_list))
