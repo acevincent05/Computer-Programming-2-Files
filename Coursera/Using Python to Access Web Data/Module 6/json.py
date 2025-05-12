e20829bdde9a21ce1502c34e0344c17f95768a60
Ace Gamitin <gamitinacevincent@gmail.com>
2025-05-12 21:11:49 +0800

    #

--- Coursera/Using Python to Access Web Data/Module 6/json.py
+++ Coursera/Using Python to Access Web Data/Module 6/json.py
@@ -9,13 +9,5 @@ data = response.json()
 
 num_list = []
 
-for num in parsed_url:
+for num in data:
     num_list.append('comments'['num'])
-
-info = json.loads(data)
-print('User count:', len(info))
-
-for item in info:
-    print('Name', item['name'])
-    print('Id', item['id'])
-    print('Attribute', item['x'])
