752fedd088dda350a85a04b5fdf4c871da7db09d
Ace Gamitin <gamitinacevincent@gmail.com>
2025-05-12 21:06:51 +0800

    #

--- Coursera/Using Python to Access Web Data/Module 6/json.py
+++ Coursera/Using Python to Access Web Data/Module 6/json.py
@@ -1,8 +1,10 @@
 import json
 import requests
+from urllib.parse import urlparse, parse_qs
 
 url = "https://py4e-data.dr-chuck.net/comments_2198753.json"
 
+parsed_url = urlparse(url)
 
 info = json.loads(data)
 print('User count:', len(info))
